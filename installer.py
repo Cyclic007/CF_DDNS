5f2011e2e6cffb2e901f9983aff8496e87e76185
Cyclic <cyclic007dev@gmail.com>
2025-01-01 10:02:45 -0600

    added IPv6 capabiolitys

--- installer.py
+++ installer.py
@@ -8,19 +8,8 @@ print(os.getcwd())
 if OS.lower()[1] == "l":
     #this means it is a luinux system
     print("adding to cron")
-    os.system("""sudo echo \"[Unit]\\n
-    Description=Updates your DNS records\\n
-    After=network.target\\n
-    
-    [Service]\\n
-    Type=idle\\n
-    Restart=on-failure\\n
-    User=root\\n
-    ExecStart=/usr/bin/python """
-              +os.getcwd()+"main.py"+"""\\n
-    RestartSec=30
-    [Install]\\n
-    WantedBy=multi-user.target\" > /lib/systemd/system/CF_DDNS.service""")
-    os.system("sudo chmod 644 /lib/systemd/system/CF_DDNS.service")
-    os.system("sudo systemctl daemon-reload")
-    os.system("sudo systemctl enable CF_DDNS.service")
+    script = open("CF_DDNS.sh","w")
+    script.write("python3 ./mian.py")
+    os.system("chmod +x CF_DDNS.sh")
+
+    os.system("crontab -l -u root | cat - "+os.getcwd()+" | crontab -u root -")
--- mian.py
+++ mian.py
@@ -3,9 +3,20 @@ import os
 import cloudflare
 import requests
 
+import socket
 
+def get_ipv6_address():
+    """Gets the current IPv6 address of the machine."""
+
+    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    try:
+        s.connect(('2001:4860:4860::8888', 80)) # Google's public DNS IPv6 address
+        return s.getsockname()[0]
+    except Exception:
+        return None
+    finally:
+        s.close()
 configfile_name = "CF_DDNS_Config.ini"
-
 # Check if there is already a configurtion file
 if not(os.path.isfile(configfile_name)):
     # Create the configuration file as it doesn't exist yet
@@ -17,9 +28,10 @@ if not(os.path.isfile(configfile_name)):
     config.set("UserInfo", "Email", "example@google.com")
     config.set("UserInfo","ApiKey","<inputApiKey>")
     config.add_section("DNSDeets")
-    config.set("DNSDeets","DNSRecordID","changeThis")
+    config.set("DNSDeets","DNSRecordIDs (separate by commas AAAA record goes first if using IPv6)","changeThis")
     config.set("DNSDeets", "zoneID","ChangeThis")
     config.set("DNSDeets","domainName","cookiecamp.org")
+    config.set("DNSDeets","IP version(IPv4, IPv6 or both)","IPv4")
     config.add_section("debug")
     config.set("debug","ipv4_Service", "http://ipinfo.io/ip")
     config.write(cfgfile)
@@ -31,38 +43,84 @@ configur.read("CF_DDNS_Config.ini")
 
 email = configur.get("UserInfo","Email")
 apiKey = configur.get("UserInfo","ApiKey")
-dnsRecordID = configur.get("DNSDeets","DNSRecordID")
+dnsRecordIDs = configur.get("DNSDeets","DNSRecordIDs (separate by commas AAAA record goes first if using IPv6)").split(",")
 zoneID = configur.get("DNSDeets","zoneID")
 ipv4Service = configur.get("debug","ipv4_Service")
 domainName = configur.get("DNSDeets","domainName")
+ipVersion = configur.get("DNSDeets","IP version(IPv4, IPv6 or both)")
+if ipVersion == "IPv6" or ipVersion == "both":
+    ipv6 = get_ipv6_address()
+    if ipv6:
+        print("IPv6 address:", ipv6)
+    else:
+        print("No IPv6 address found.")
+    client = cloudflare.Cloudflare(
+        api_token=apiKey
+    )
+    responce = client.dns.records.get(
+        dns_record_id=dnsRecordIDs[0],
+        zone_id=zoneID,
+    )
+    endIPindex = str(responce).find(", name=")
+
+    fetchedIP = "".join(str(responce).split())[17:endIPindex-1]
+    print(fetchedIP)
+    if fetchedIP != ipv6:
+        responce = client.dns.records.edit(
+            dns_record_id=dnsRecordIDs[0],
+            zone_id=zoneID,
+            type="AAAA",
+            name=domainName,
+            content=ipv6,
+        )
+
 # print(email)
 # print(dnsRecordID)
-r = requests.get(ipv4Service)
-try:
-    ipv4 = r.text
-except:
-    print("are you connected to the internet")
-    exit()
-print(ipv4)
+if ipVersion == "IPv4" or ipVersion == "both":
+    r = requests.get(ipv4Service)
+    try:
+        ipv4 = r.text
+    except:
+        print("are you connected to the internet")
+        exit()
+    print(ipv4)
 
-client = cloudflare.Cloudflare(
-    api_token=apiKey
-)
-responce = client.dns.records.get(
-    dns_record_id=dnsRecordID,
-    zone_id=zoneID,
-)
-
-endIPindex = str(responce).find(", name=")
-
-fetchedIP = "".join(str(responce).split())[17:endIPindex-1]
-print(fetchedIP)
-if fetchedIP != ipv4:
-    responce = client.dns.records.edit(
-        dns_record_id=dnsRecordID,
-        zone_id=zoneID,
-        type="A",
-        name=domainName,
-        content=ipv4,
+    client = cloudflare.Cloudflare(
+        api_token=apiKey
     )
+    if ipVersion == "both":
+        responce = client.dns.records.get(
+            dns_record_id=dnsRecordIDs[1],
+            zone_id=zoneID,
+        )
 
+        endIPindex = str(responce).find(", name=")
+
+        fetchedIP = "".join(str(responce).split())[17:endIPindex-1]
+        print(fetchedIP)
+        if fetchedIP != ipv4:
+            responce = client.dns.records.edit(
+                dns_record_id=dnsRecordIDs[1],
+                zone_id=zoneID,
+                type="A",
+                name=domainName,
+                content=ipv4,
+            )
+    else:
+        responce = client.dns.records.get(
+            dns_record_id=dnsRecordIDs[0],
+            zone_id=zoneID,
+        )
+
+        endIPindex = str(responce).find(", name=")
+
+        fetchedIP = "".join(str(responce).split())[17:endIPindex-1]
+        print(fetchedIP)
+        if fetchedIP != ipv4:
+            responce = client.dns.records.edit(
+                dns_record_id=dnsRecordIDs[0],
+                zone_id=zoneID,
+                type="A",
+                name=domainName,
+                content=ipv4,
+            )
